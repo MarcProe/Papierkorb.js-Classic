ae97487361d8ae4bc5b32bf370b2d2edbfb89f10
Marcus Proest <marcus@proest.net>
2021-02-13 17:26:51 +0100

    change preview aspect ratio

--- public/js/views/docs.js
+++ public/js/views/docs.js
@@ -1,17 +1,26 @@
 $(document).ready(function () {
-    $('.hoverover').on('mouseenter', function (evt) {
+    $(".hoverover").on("mouseenter", function (evt) {
         let src = this;
-        let left = $(window).width() / 3.;
+        let left = $(window).width() / 3;
 
-        let hoverimagesel = $('#hoverimage');
+        let hoverimagesel = $("#hoverimage");
 
-        const previewUrl = hoverimagesel.attr('data-base');
+        const previewUrl = hoverimagesel.attr("data-base");
+
+        console.log(hoverimagesel);
 
         hoverimagesel
-            .html('<img style="position:fixed; top:10px" height="' + ($(window).height() * 0.85) + '" src="' + previewUrl + src.id + '.0.thumb.png" />')
-            .css({left: left, top: 100})
+            .html(
+                '<img style="position:fixed; top:10px" width="' +
+                    $(window).width() * 0.5 +
+                    'px" src="' +
+                    previewUrl +
+                    src.id +
+                    '.0.thumb.png" />'
+            )
+            .css({ left: left, top: 100 })
             .show();
-        $(this).on('mouseleave', function () {
+        $(this).on("mouseleave", function () {
             hoverimagesel.hide();
         });
     });
