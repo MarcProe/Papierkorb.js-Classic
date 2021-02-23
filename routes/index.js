ff9070594e2582db349678ca8cdb1a6561409e2b
Marcus Proest <marcus@proest.net>
2021-02-23 20:31:24 +0100

    lots of changes

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,19 @@
-let express = require('express');
-let router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-let render = require('../modules/render.js');
+const render = require("../modules/render.js");
 
-let config = require('config');
-let conf = config.get('conf');
+const config = require("config");
+const conf = config.get("conf");
 
-router.get('/', function(req, res, next) {
-    let title = 'Willkommen im Papierkorb';
-    render.rendercallback(null, req, res, 'index', {}, conf, title);
+router.get("/", function (req, res, next) {
+    if (req.query.bootstrap === "1") {
+        req.session.bootstrap = true;
+    } else {
+        req.session.bootstrap = false;
+    }
+    const title = "Willkommen im Papierkorb";
+    render.rendercallback(null, req, res, "index", {}, conf, title);
 });
 
 module.exports = router;
