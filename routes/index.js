715aaf5860858881c2e911b4469a4344ce7c9155
Marcus Proest <marcus@proest.net>
2022-11-26 13:36:26 +0100

    remove bootstrap

--- routes/index.js
+++ routes/index.js
@@ -7,22 +7,6 @@ const config = require("config");
 const conf = config.get("conf");
 
 router.get("/", function (req, res, next) {
-    if (req.query.bootstrap === "1") {
-        req.session.bootstrap = true;
-        res.cookie("bootstrap", true, {
-            maxAge: 900000,
-            httpOnly: true,
-        });
-    } else if (req.query.bootstrap === "0") {
-        req.session.bootstrap = false;
-        res.cookie("bootstrap", false, {
-            maxAge: 900000,
-            httpOnly: true,
-        });
-    } else {
-        req.session.bootstrap = req.cookies.bootstrap === "true";
-    }
-
     const title = "Willkommen im Papierkorb";
     render.rendercallback(null, req, res, "index", {}, conf, title);
 });
